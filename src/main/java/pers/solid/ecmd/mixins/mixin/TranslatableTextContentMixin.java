a8af2e838f9028ba3820ef940c28c510b0d39314
SolidBlock <hulunabc@163.com>
2025-01-13 01:38:34 +0800

    Revert "进行调试"
    This reverts commit 3490df167cd34185037cdd82a5a0c7f607dbfd9c.

--- src/main/java/pers/solid/ecmd/mixins/mixin/TranslatableTextContentMixin.java
+++ src/main/java/pers/solid/ecmd/mixins/mixin/TranslatableTextContentMixin.java
@@ -1,7 +1,6 @@
 package pers.solid.ecmd.mixins.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import com.mojang.logging.LogUtils;
 import com.mojang.serialization.MapCodec;
 import net.minecraft.text.TranslatableTextContent;
 import org.spongepowered.asm.mixin.Mixin;
@@ -12,10 +11,6 @@ import pers.solid.ecmd.util.mixin.TranslatableTextContentMixinHelper;
 public abstract class TranslatableTextContentMixin {
   @ModifyExpressionValue(method = "<clinit>", at = @At(value = "INVOKE", target = "Lcom/mojang/serialization/codecs/RecordCodecBuilder;mapCodec(Ljava/util/function/Function;)Lcom/mojang/serialization/MapCodec;", remap = false))
   private static MapCodec<TranslatableTextContent> modifyCodec(MapCodec<TranslatableTextContent> original) {
-    if (true) {
-      LogUtils.getLogger().info("Tried modify translatable text content codec but I skipped");
-      return original;
-    }
     return TranslatableTextContentMixinHelper.modifyTranslatableCodec(original);
   }
 }
